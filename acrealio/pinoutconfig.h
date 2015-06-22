a0b8374d4a0d6c5f91a4f59c5fcf8a40f020690e
itjesse <jesse@itjesse.cn>
2015-06-23 04:12:34 +0800

    修改键位

--- acrealio/pinoutconfig.h
+++ acrealio/pinoutconfig.h
@@ -1,14 +1,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define GAMETYPE 1               //0:pop'n with card dispenser 1: pop'n, drummania(1 reader) 2:iidx/ddr sd/gf(2readers) 3:jubeat (1reader+Ledboard) 4: sdvx (1reader+ioboard) 5: ddr hd (2readers + ??? board)
+#define GAMETYPE 4               //0:pop'n with card dispenser 1: pop'n, drummania(1 reader) 2:iidx/ddr sd/gf(2readers) 3:jubeat (1reader+Ledboard) 4: sdvx (1reader+ioboard) 5: ddr hd (2readers + ??? board)
 
 #define RFID_BAUD 115200		//Baud rate for RFID Module
 
 #define RFID_MODULE1 1			//Rfid module used for reader1, 1:SL015M 2:RR10
 #define RFID_MODULE2 1			//Rfid module used for reader2, 1:SL015M 2:RR10
 
-#define SDVX_VOL_SENS 7        //Sensitivity for SDVX Volume buttons
+#define SDVX_VOL_SENS 1        //Sensitivity for SDVX Volume buttons
 
 
 //input pins for sdvx controls
@@ -20,7 +20,7 @@
 #define BT_D A5
 #define BT_START 50
 #define BT_TEST 52
-#define BT_SVC 40
+#define BT_SVC 53
 
 //input pins for volume encoders (phase A and phase B for each)
 #define VOLR_A 42
@@ -29,7 +29,7 @@
 #define VOLL_B 48
 
 //pins for sdvx lights (outputs)
-#define LT_START 53
+#define LT_START 40
 #define LT_A 51
 #define LT_B 49
 #define LT_C 47
@@ -54,7 +54,7 @@
 
 
 //pins for card reader 1 keypad (colls ABC, rows 1234)
-/* 
+/*
 ### Keypad 3x4 Matrix ###
 
  (7)---(8)---(9)---> ROW 4
@@ -69,17 +69,17 @@
 COL C  COL B  COL A
 
 */
-#define K1_A 27
-#define K1_B 25
-#define K1_C 23
-#define K1_1 29
-#define K1_2 31
-#define K1_3 33
-#define K1_4 35
+#define K1_A 22
+#define K1_B 24
+#define K1_C 26
+#define K1_1 28
+#define K1_2 30
+#define K1_3 32
+#define K1_4 34
 
 
 //pins for card reader 1 RFID Module
-#define R1_DET 20 
+#define R1_DET 20
 #define R1_SER Serial1
 
 //pins for card reader 2 keypad (colls ABC, rows 1234)
