32c2ee2ee8bb4bf4764fb3f8c3251d596dce754c
itjesse <jesse@itjesse.cn>
2015-06-23 04:12:43 +0800

    魔改固定uid

--- acrealio/SL015M.cpp
+++ acrealio/SL015M.cpp
@@ -14,10 +14,10 @@ void SL015M::setPins(int sensor, HardwareSerial* serialid)
 {
     rfSENSOR = sensor;
     rfSerial = serialid;
-	
+
     pinMode(rfSENSOR,INPUT);                            // Rfid sensor
     rfSerial->begin(RFID_BAUD);
-    
+
     pinset=true;
 
 }
@@ -28,84 +28,19 @@ void SL015M::update()
     return;
   if(!readcmd)
     return;
-  
+
     if(digitalRead(rfSENSOR)==LOW)             // card presence
     {
-        
-        if(!rfcmdsent)
-        {
-        
-          rfSerial->write(0xBA);                  // pream
-          rfSerial->write(0x02);                  // size
-          rfSerial->write(0x31);                  // rfidcommand: get tag info
-          rfSerial->write(0xBA ^ 0x02 ^ 0x31);    // checksum
-          
-          rf_i = 0;
-          rfcmdsent= true;
-          timesent = millis();
-        }
-        else // if command has been sent, let's read answer
-        {
-
-          if(rfSerial->available()>0)
-          {
-            
-            rfidp[rf_i] = rfSerial->read();
-            rf_i++;
-            
-            if(rfidp[0] != 0xBD)                 // restart filling buffer from begining until received byte is 0xBD (preamb)
-              rf_i = 0;
-            
-            if(rf_i>2)                           // if we know paquet length
-            {
-              
-              byte length = rfidp[1];
-              if(rf_i >= length + 2)             // if paquet complete
-              {
-          
-            // checksum check
-                byte chk = 0x00; 
-                for(int i=0;i< length+1;i++)
-                  chk ^= rfidp[i];
-                
-                if(chk != rfidp[length+1])
-                {
-                  rfcmdsent = false;             // checksum error, send cmd again
-                }
-                else
-                {
-                  if(rfidp[3]!=0x00)             // Operation not succeeded
-                  {
-                    rfcmdsent = false;           // error, send again
-                  }
-                  else
-                  {
-            
-            //success, let's copy the uid
-                    for(int i=0;i<8;i++)
-                    {
-                      uid[i] = rfidp[11-i];
-                    }
-            
-                    if(uid[0] == 0xE0 && uid[1] == 0x04)  // if correct konami card
-                      card = 1;
-                    else
-                      card = 0;
-                      
-                    readcmd = false;//reading finished (card found)
-
-                  }
-                }
-              }
-            }
-          }
-          else 
-          {
-            // command sent, but nothing to receive
-            if(millis() - timesent >500)         // if last command was sent more than 500ms ago, send a new one
-              rfcmdsent = false;
-          }
-        }
+        uid[0] = 0xE0;
+        uid[1] = 0x04;
+        uid[2] = 0x01;
+        uid[3] = 0x00;
+        uid[4] = 0x3C;
+        uid[5] = 0x94;
+        uid[6] = 0x78;
+        uid[7] = 0xFF;
+        card = 1;
+        readcmd = false;
     }
     else // if no tag in range
     {
